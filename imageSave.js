dd047a66f273743bd8ef0819cccf48fc550a0c51
Erlend Tobiassen <erlend.tobiassen@gmail.com>
2019-04-22 16:49:58 +0200

    token move support

--- imageSave.js
+++ imageSave.js
@@ -5,11 +5,29 @@ onmessage = evt => {
     const [id, config, image] = evt.data;
     console.debug('Save message received.');
 
-    Promise.all([
+    const promise = Promise.all([
         localforage.setItem(`fow.${id}`, image),
         localforage.setItem(`config.${id}`, config),
     ]).then(() => {
         console.log('Saved!');
-        close();
-    }).catch(console.error);
+    }).catch(err => {
+        console.error("Error during saving to localforage");
+        console.error(err);
+    }).finally(() => close());
+
+    console.log(evt);
+    console.log(promise);
+
+    setTimeout(() => {
+        console.log("Eh?");
+    }, 5000);
+}
+
+onerror = err => {
+    console.error("WebWorker error");
+    console.error(err);
+}
+
+onclose = () => {
+    console.log("Closing WebWorker");
 }
